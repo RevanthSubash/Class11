407cb69f24a222ea7e9a89b956955106cbabbb32
RevanthSubash <112666083+RevanthSubash@users.noreply.github.com>
2022-09-09 17:39:51 -0500

    Add files via upload

---
+++ C-11_teacher_activity-1-main/C-11_teacher_activity-1-main/sketch.js
@@ -0,0 +1,23 @@
+var marks = [30,40,45,35];
+function average(){
+var sum=0
+for(var i=0; i<marks.length; i++)
+{
+  sum = sum+marks[i]
+
+}
+var avg= sum/marks.length
+console.log(avg)
+}
+
+
+
+
+function setup() {
+  createCanvas(400, 400);
+average()
+}
+
+function draw() {
+  background(150);
+}
